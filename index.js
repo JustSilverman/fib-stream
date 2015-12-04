ed3deb252f838317a3450423765021bc679307f4
justins <justins@wealthfront.com>
2015-12-04 15:46:52 -0800

    Update index.js to use limit and accommodate changes to appendString (now a generator)

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 'use strict';
 
+var transformStreamFrom = require('./src/transform-stream-from');
 var readStreamFrom = require('./src/read-stream-from');
 var fibGenerator = require('./src/fib-generator');
-var take = require('./src/take');
+var limit = require('./src/limit');
 var appendString = require('./src/append-string');
 
 var elementsInSequence = process.argv[2];
 
 readStreamFrom(fibGenerator())
-  .pipe(take(elementsInSequence))
-  .pipe(appendString('\n'))
+  .pipe(transformStreamFrom(limit(elementsInSequence)))
+  .pipe(transformStreamFrom(appendString('\n')))
   .pipe(process.stdout);
